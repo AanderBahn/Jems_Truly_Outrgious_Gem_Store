eace9b6233f067b08030c56c53106b081c7145ed
Alex B <AanderBahn@gmail.com>
2017-03-31 18:28:49 -0500

    First Setup and Push
    Just getting thing ready =

---
+++ Jems_Gem_Store/Jems_Gem_Store/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jems_Gem_Store.MvcApplication" Language="C#" %>
---
+++ Jems_Gem_Store/Jems_Gem_Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jems_Gem_Store.Startup))]
+namespace Jems_Gem_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
