fe393fbe162dadbadb7cb68c0a1bba309eb1d9bb
Alex B <AanderBahn@gmail.com>
2017-04-04 17:19:59 -0500

    Setup part 2
    The Ng-Repeater and other parts added

--- Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
+++ Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
@@ -1,1 +1,22 @@
-﻿var app = angular.module('store', []);
+﻿(function () {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.goods = gems;
+    });
+
+    var gems = [
+    {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'This 12 x 5 faceted crystal is great.',
+        soldOut: false,
+    },
+    {
+        name: 'LockNar',
+        price: 7.47,
+        description: 'A luminus green crystal that shines with the glee of greater spheres.',
+        soldOut: false,
+    }//end of products
+    ];
+})();
