36b1fd5af39cfde01aa86ae97739236672a9d7b2
Alex B <AanderBahn@gmail.com>
2017-04-05 15:40:48 -0500

    Images and other added
    Images added to project and minor refractoring

--- Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
+++ Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
@@ -8,15 +8,37 @@
     var gems = [
     {
         name: 'Dodecahedron',
-        price: 2.95,
+        price: 2,
         description: 'This 12 x 5 faceted crystal is great.',
         soldOut: false,
+        images: [
+            {
+                full: '1D12_Black.jpg',
+            },
+            {
+                full: 'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/1D12_Blue.jpg',
+            },
+            {
+                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/1D12_green.png',
+            },
+        ],
     },
     {
         name: 'LockNar',
         price: 7.47,
         description: 'A luminus green crystal that shines with the glee of greater spheres.',
         soldOut: false,
+        images: [
+            {
+                full:'LocNar_1.jpg',
+            },
+            {
+                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/LocNar_2.jpg',
+            },
+            {
+                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/LocNar_3.jpg',
+            },
+        ],
     }//end of products
     ];
 })();
