bb0ce4e54ceccface5dc4f69a99113af747f6a60
Alex B <AanderBahn@gmail.com>
2017-04-10 16:49:30 -0500

    Reviews section Added
    The Reviews section has been started.

--- Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
+++ Jems_Gem_Store/Jems_Gem_Store/Scripts/app.js
@@ -1,6 +1,8 @@
 ﻿(function () {
     var app = angular.module('store', []);
 
+
+    //// Manages the Products and Goods ///
     app.controller('StoreController', function () {
         this.goods = gems;
     });
@@ -13,15 +15,30 @@
         soldOut: false,
         images: [
             {
-                full: '1D12_Black.jpg',
+                full: '~/Content/images/1D12_Black.jpg',
+                thumb: ''
             },
             {
-                full: 'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/1D12_Blue.jpg',
+                full: '~/Content/images/1D12_Blue.jpg',
+                thumb: ''
             },
             {
-                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/1D12_green.png',
+                full: '~/Content/images/1D12_green.png',
+                thumb: ''
             },
         ],
+        reviews: [
+            {
+                stars: 5,
+                body: "This is so rad!!!",
+                author: "bob@gmail.com"
+            },
+            {
+                stars: 1,
+                body: "Kinda...Meh. D20 is where its at",
+                author: "larry@gmail.com"
+            },
+        ]
     },
     {
         name: 'LockNar',
@@ -30,15 +47,36 @@
         soldOut: false,
         images: [
             {
-                full:'LocNar_1.jpg',
+                full: '~/Content/images/LocNar_1.jpg',
+                thumb:''
             },
             {
-                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/LocNar_2.jpg',
+                full: '~/Content/images/LocNar_2.jpg',
+                thumb: ''
             },
             {
-                full:'C:\Users\black\Desktop\Jems_Truly_Outrgious_Gem_Store\Jems_Gem_Store\Jems_Gem_Store\Content/images/LocNar_3.jpg',
+                full: '~/Content/images/LocNar_3.jpg',
+                thumb: ''
             },
         ],
     }//end of products
     ];
+
+    //// Managing Product and and Info Tabs
+    app.controller("PanelController", function () {
+        //Sets the tab to 1 on load
+        this.tab = 1;
+
+        //Makes the tab clicked, the tab that's shown
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        //
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+
+    });///End of the Panel Controller
+
 })();
